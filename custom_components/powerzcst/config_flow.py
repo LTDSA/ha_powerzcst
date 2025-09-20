9b93498970b797df05b279e0f575d6b044d79074
LTDSA <ltdsa@ltdsa.cn>
2025-09-20 21:22:43 +0800

    perf: show error message when login failed

--- custom_components/powerzcst/config_flow.py
+++ custom_components/powerzcst/config_flow.py
@@ -30,21 +30,33 @@ async def validate_input(hass: HomeAssistant, data: dict[str, Any]) -> dict[str,
 
     Data has the keys from STEP_USER_DATA_SCHEMA with values provided by the user.
     """
-    # TODO validate the data can be used to set up a connection.
-
-    # If your PyPI package is not built with async, pass your methods
-    # to the executor:
-    # await hass.async_add_executor_job(
-    #     your_validate_func, data["username"], data["password"]
-    # )
-
-    # If you cannot connect:
-    # throw CannotConnect
-    # If the authentication is wrong:
-    # throw InvalidAuth
-
-    # Return info that you want to store in the config entry.
-    return {"title": f"PowerZCST: {data[CONF_USERNAME]}"}
+    import aiohttp
+    
+    username = data[CONF_USERNAME]
+    password = data[CONF_PASSWORD]
+    endpoint = data.get(CONF_ENDPOINT, DEFAULT_ENDPOINT)
+    
+    login_url = f"{endpoint}/login/?username={username}&password={password}"
+    
+    async with aiohttp.ClientSession() as session:
+        try:
+            async with session.get(login_url) as response:
+                if response.status != 200:
+                    raise CannotConnect
+                
+                login_data = await response.json()
+                
+                # 检查接口返回值的 code 字段
+                if login_data.get("code") != 200:
+                    # 如果 code 不为 200，使用 msg 字段作为错误信息
+                    error_msg = login_data.get("msg", "未知错误")
+                    raise ApiError(error_msg)
+                
+                # 登录成功
+                return {"title": f"PowerZCST: {username}"}
+                
+        except aiohttp.ClientError:
+            raise CannotConnect
 
 
 class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
@@ -57,6 +69,8 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
     ) -> FlowResult:
         """Handle the initial step."""
         errors: dict[str, str] = {}
+        error_msg = None
+        
         if user_input is not None:
             try:
                 info = await validate_input(self.hass, user_input)
@@ -64,6 +78,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 errors["base"] = "cannot_connect"
             except InvalidAuth:
                 errors["base"] = "invalid_auth"
+            except ApiError as err:
+                # 使用 API 返回的错误信息
+                errors["base"] = "api_error"
+                error_msg = err.error_msg
             except Exception:  # pylint: disable=broad-except
                 _LOGGER.exception("Unexpected exception")
                 errors["base"] = "unknown"
@@ -71,7 +89,10 @@ class ConfigFlow(config_entries.ConfigFlow, domain=DOMAIN):
                 return self.async_create_entry(title=info["title"], data=user_input)
 
         return self.async_show_form(
-            step_id="user", data_schema=STEP_USER_DATA_SCHEMA, errors=errors
+            step_id="user", 
+            data_schema=STEP_USER_DATA_SCHEMA, 
+            errors=errors,
+            description_placeholders={"error_msg": error_msg} if error_msg else None
         )
 
 
@@ -81,3 +102,12 @@ class CannotConnect(HomeAssistantError):
 
 class InvalidAuth(HomeAssistantError):
     """Error to indicate there is invalid auth."""
+
+
+class ApiError(HomeAssistantError):
+    """Error to indicate there is an API error."""
+    
+    def __init__(self, error_msg: str):
+        """Initialize with error message."""
+        super().__init__()
+        self.error_msg = error_msg
